46bed913d140fb2ca36d2a6b17dd0e452ef72e91
Marciano Schildmeijer <marciano@kabisa.nl>
2021-12-07 21:17:50 +0100

    Filter bar UI cvhanges.

--- components/FilterBar/index.tsx
+++ components/FilterBar/index.tsx
@@ -3,31 +3,32 @@ import Button from "../Button";
 import styles from "./styles.module.css";
 
 const FilterBar: FC = () => (
-	<aside className={styles.filterbar}>
-		<h3 className={styles.filterTitle}>Algemene filters</h3>
-		<ul className={styles.filterList}>
-			<li>
-				<Button isActive={true} variant="flatBig">
-					meest recent
-				</Button>
-			</li>
-		</ul>
-		<h3 className={styles.filterTitle}>Lift filters</h3>
-		<ul className={styles.filterList}>
-			<li>
-				<Button variant="flatBig">beste squat</Button>
-			</li>
-			<li>
-				<Button variant="flatBig">beste bench press</Button>
-			</li>
-			<li>
-				<Button variant="flatBig">beste deadlift</Button>
-			</li>
-			<li>
-				<Button variant="flatBig">beste totaal</Button>
-			</li>
-		</ul>
-	</aside>
+	<section className={styles.filterbarContainer}>
+		<h3 className={styles.filterTitle}>Filters</h3>
+		<aside className={styles.filterbar}>
+			<ul className={styles.filterList}>
+				<li>
+					<Button isActive={true} variant="flatBig">
+						meest recent
+					</Button>
+				</li>
+			</ul>
+			{/* <ul className={styles.filterList}>
+				<li>
+					<Button variant="flatBig">beste squat</Button>
+				</li>
+				<li>
+					<Button variant="flatBig">beste bench press</Button>
+				</li>
+				<li>
+					<Button variant="flatBig">beste deadlift</Button>
+				</li>
+				<li>
+					<Button variant="flatBig">beste totaal</Button>
+				</li>
+			</ul> */}
+		</aside>
+	</section>
 );
 
 export default FilterBar;
